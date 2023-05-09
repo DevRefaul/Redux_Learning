19070117b987325dca6a74f88aded2f37fe16035
DevRefaul <devrefaul@gmail.com>
2023-05-09 20:21:55 +0600

    updated the folder structure for learning other concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
-import { useReducer } from "react";
-import reducer, { initialState } from "./Reducer/reducer";
+import Counter from "./Components/Counter";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
-    <section className="flex justify-center items-center h-[100vh]">
-      <div>
-        <h2 className="my-6 text-4xl font-semibold text-center">Counter</h2>
-        <p className="my-6">
-          Current Count is{" "}
-          <span className="text-3xl font-semibold text-rose-500">
-            {state.count}
-          </span>
-        </p>
-        <div className="flex justify-center">
-          <span>
-            <button
-              onClick={() => dispatch({ type: "Increment" })}
-              className="px-2 py-1 mx-2 bg-rose-500 text-2xl text-white"
-            >
-              +
-            </button>
-          </span>
-          <span>
-            <button
-              onClick={() => dispatch({ type: "Decrement" })}
-              className="px-2 py-1 mx-2 bg-rose-500 text-2xl text-white"
-            >
-              -
-            </button>
-          </span>
-        </div>
-      </div>
-    </section>
+    <>
+      <Counter />
+    </>
   );
 }
 

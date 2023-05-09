6984569e766a981d6fae47e9ff5aa8fe643c6d40
DevRefaul <devrefaul@gmail.com>
2023-05-09 19:46:18 +0600

    updated the layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,29 @@ function App() {
   return (
     <section className="flex justify-center items-center h-[100vh]">
       <div>
-        <h2 className="my-6">Counter</h2>
+        <h2 className="my-6 text-4xl font-semibold text-center">Counter</h2>
         <p className="my-6">
           Current Count is{" "}
           <span className="text-3xl font-semibold text-rose-500">{state}</span>
         </p>
-        <span>
-          <button
-            onClick={() => setState(state + 1)}
-            className="px-2 py-1 mx-2 bg-rose-500 text-2xl text-white"
-          >
-            +
-          </button>
-        </span>
-        <span>
-          <button
-            onClick={() => setState(state - 1)}
-            className="px-2 py-1 mx-2 bg-rose-500 text-2xl text-white"
-          >
-            -
-          </button>
-        </span>
+        <div className="flex justify-center">
+          <span>
+            <button
+              onClick={() => setState(state + 1)}
+              className="px-2 py-1 mx-2 bg-rose-500 text-2xl text-white"
+            >
+              +
+            </button>
+          </span>
+          <span>
+            <button
+              onClick={() => setState(state - 1)}
+              className="px-2 py-1 mx-2 bg-rose-500 text-2xl text-white"
+            >
+              -
+            </button>
+          </span>
+        </div>
       </div>
     </section>
   );
